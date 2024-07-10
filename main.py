a38861b2ce873c2ee4798f1d47ed42496f5fa066
Juanito Ortega <47042583+JuanitoOrtega@users.noreply.github.com>
2024-07-10 09:29:32 -0400

    gestion de cors agregado

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from users.routes import router as guest_router, user_router
 from auth.route import router as auth_router
 from core.security import JWTAuth
@@ -7,6 +8,16 @@ from starlette.middleware.authentication import AuthenticationMiddleware
 
 
 app = FastAPI()
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las solicitudes de origen
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
 app.include_router(guest_router)
 app.include_router(user_router)
 app.include_router(auth_router)
